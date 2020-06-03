0ea71c9737701a48e1292b29d38892442a23b19a
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-06-03 22:26:15 +0500

    I returned

--- src/js/consts.js
+++
@@ -1,6 +0,0 @@
-import {getRandom} from './utils.js';
-
-export const COLOURS = [`black`, `yellow`, `blue`, `green`, `pink`];
-
-export const NUMBER_OF_CARDS = getRandom(25, 3);
-export const NUMBER_LOAD_CARD = 8;
