df10d8267f163301148c4fa0c86ec89a9e0ca415
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-06-16 12:46:19 +0500

    update date, create change Date and repeating days

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,14 +1,3 @@
-const castTimeFormat = (value) => {
-  return value < 10 ? `0${value}` : String(value);
-};
-
-const formatTime = (date) => {
-  const hours = castTimeFormat(date.getHours() % 12);
-  const minutes = castTimeFormat(date.getMinutes());
-
-  return `${hours}:${minutes}`;
-};
-
 const getRandomArrayItem = (array) => {
   const randomIndex = getRandomIntegerNumber(0, array.length);
 
@@ -39,7 +28,6 @@ const onEscKeyDown = (evt, action) => {
 };
 
 export {
-  formatTime,
   getRandomArrayItem,
   getRandomDate,
   onEscKeyDown,
