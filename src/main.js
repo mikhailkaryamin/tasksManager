7a1dad9534c2147b8daa89464dc92c1e87ed15a0
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-06-20 16:34:14 +0500

    create charts statistics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import BoardController from './controllers/board.js';
 import MenuController from './controllers/menu.js';
 import Filters from './controllers/filters.js';
 import Tasks from './models/tasks.js';
-import Statistic from './components/statistic.js';
+import Statistic from './components/statistics.js';
 import {generateTasks} from './mock/task.js';
 import {
   CONTROL_MENU_ID_PREFIX,
@@ -24,18 +24,26 @@ const onPressButtonMenu = (evt) => {
   }
 };
 
+const dateTo = new Date();
+const dateFrom = (() => {
+  const d = new Date(dateTo);
+  d.setDate(d.getDate() - 7);
+  return d;
+})();
+
 const mainEl = document.querySelector(`.main`);
 const mainControlEl = mainEl.querySelector(`.main__control`);
 const menuController = new MenuController(mainControlEl, onPressButtonMenu);
 const boardController = new BoardController(mainEl, tasksModel);
 const filtersController = new Filters(mainEl, tasksModel);
-const statisticComponent = new Statistic();
+const statisticComponent = new Statistic({tasks: tasksModel, dateFrom, dateTo});
 
 menuController.render();
 filtersController.render();
 boardController.render();
 
 render(mainEl, statisticComponent);
+statisticComponent.hide();
 
 const controlPagesMenu = (pageName) => {
   switch (pageName) {
